0a67503973f258248496de730899b4e51ed5bbb1
Alp <hizalper@gmail.com>
2024-04-18 15:55:33 -0400

    responses.txt added and fillResponseMap() is updated.

--- Responder.java
+++ Responder.java
@@ -26,6 +26,9 @@ public class Responder
     // The name of the file containing the default responses.
     private static final String FILE_OF_DEFAULT_RESPONSES = "default.txt";
     private Random randomGenerator;
+    
+    private static final String FILE_OF_RESPONSES = "responses.txt";
+
 
     /**
      * Construct a Responder
@@ -60,60 +63,49 @@ public class Responder
         // we cannot think of anything else to say...)
         return pickDefaultResponse();
     }
-
+    
     /**
-     * Enter all the known keywords and their associated responses
-     * into our response map.
+     * Modify the method to read responses from a file instead of hardcoding them.
+     * Each entry in the file should have:
+     * First line: keyword(s) (comma-separated values)
+     * Following lines: response until a blank line
      */
     private void fillResponseMap()
     {
-        responseMap.put("crash", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("crashes", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("slow", 
-                        "I think this has to do with your hardware. Upgrading your processor\n" +
-                        "should solve all performance problems. Have you got a problem with\n" +
-                        "our software?");
-        responseMap.put("performance", 
-                        "Performance was quite adequate in all our tests. Are you running\n" +
-                        "any other processes in the background?");
-        responseMap.put("bug", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("buggy", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("windows", 
-                        "This is a known bug to do with the Windows operating system. Please\n" +
-                        "report it to Microsoft. There is nothing we can do about this.");
-        responseMap.put("macintosh", 
-                        "This is a known bug to do with the Mac operating system. Please\n" +
-                        "report it to Apple. There is nothing we can do about this.");
-        responseMap.put("expensive", 
-                        "The cost of our product is quite competitive. Have you looked around\n" +
-                        "and really compared our features?");
-        responseMap.put("installation", 
-                        "The installation is really quite straight forward. We have tons of\n" +
-                        "wizards that do all the work for you. Have you read the installation\n" +
-                        "instructions?");
-        responseMap.put("memory", 
-                        "If you read the system requirements carefully, you will see that the\n" +
-                        "specified memory requirements are 1.5 giga byte. You really should\n" +
-                        "upgrade your memory. Anything else you want to know?");
-        responseMap.put("linux", 
-                        "We take Linux support very seriously. But there are some problems.\n" +
-                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
-                        "more precise?");
-        responseMap.put("bluej", 
-                        "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
-                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
-                        "do about it, I'm afraid.");
+        Charset charset = Charset.forName("US-ASCII");
+        Path path = Paths.get(FILE_OF_RESPONSES);
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            String line;
+            String keyword = null;
+            StringBuilder responseBuilder = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                if (keyword == null) {
+                    keyword = line.trim(); // Read the keyword(s)
+                } else if (line.trim().isEmpty()) {
+                    // Finished reading the response for the current keyword
+                    responseMap.put(keyword, responseBuilder.toString());
+                    keyword = null; // Reset keyword
+                    responseBuilder.setLength(0); // Clear the response builder
+                } else {
+                    responseBuilder.append(line).append("\n"); // Append the line to the response
+                }
+            }
+            // Add the last response if it's not empty
+            if (keyword != null && responseBuilder.length() > 0) {
+                responseMap.put(keyword, responseBuilder.toString());
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.err.println("Unable to open " + FILE_OF_RESPONSES);
+        }
+        catch(IOException e) {
+            System.err.println("A problem was encountered reading " +
+                               FILE_OF_RESPONSES);
+        }
     }
+
+
+
 
     /**
      * Build up a list of default responses from which we can pick
